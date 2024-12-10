2b56d7b4e228078c152f09789230b58437c4c2ee
SurajMMH <suraj@mmh.homes>
2024-12-11 01:16:23 +0530

    Hello

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://kaushalam-todo-app-backend.onrender.com";
+const API_URL = "https://kaushalam-todo-app-backend.onrender.com/auth";
 
 const loginUser = (userData) => {
   return axios.post(`${API_URL}/login`, userData);
